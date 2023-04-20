74f0ac9a830e7c9e0c1168812142704068c99de7
SilentSamurai <souravbumbadas25@gmail.com>
2023-04-20 11:57:02 +0530

    logger & metadata api added
    Signed-off-by: SilentSamurai <souravbumbadas25@gmail.com>

--- speedy-core/src/main/java/com/github/silent/samurai/controllers/SpeedyApiController.java
+++ speedy-core/src/main/java/com/github/silent/samurai/controllers/SpeedyApiController.java
@@ -10,6 +10,7 @@ import com.google.gson.JsonElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,7 +28,7 @@ public class SpeedyApiController {
     @Autowired
     SpeedyFactory speedyFactory;
 
-    @RequestMapping(value = "/$metadata")
+    @RequestMapping(value = "/$metadata", produces = MediaType.APPLICATION_JSON_VALUE)
     public String metadata() {
         MetaModelProcessor metaModelProcessor = speedyFactory.getMetaModelProcessor();
         JsonElement jsonElement = MetaModelSerializer.serializeMetaModel(metaModelProcessor);
