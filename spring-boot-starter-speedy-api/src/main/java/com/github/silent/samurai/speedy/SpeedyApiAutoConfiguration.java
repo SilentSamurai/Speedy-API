1232fa9821eacaac923397bc6b9db5240b0563b7
SouravDas25 <souravbumbadas25@gmail.com>
2024-10-04 18:44:57 +0800

    auto config for spring boot 3

--- spring-boot-starter-speedy-api/src/main/java/com/github/silent/samurai/speedy/SpeedyApiAutoConfiguration.java
+++ spring-boot-starter-speedy-api/src/main/java/com/github/silent/samurai/speedy/SpeedyApiAutoConfiguration.java
@@ -3,12 +3,14 @@ package com.github.silent.samurai.speedy;
 import com.github.silent.samurai.speedy.controllers.SpeedyApiController;
 import com.github.silent.samurai.speedy.docs.SpeedyOpenApiCustomizer;
 import com.github.silent.samurai.speedy.interfaces.ISpeedyConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+@AutoConfiguration
 @Configuration
 @ConfigurationProperties(prefix = "speedy.api")
 @ComponentScan(basePackageClasses = SpeedyApiController.class)
