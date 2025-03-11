6334329e64920989539ab778445b4231361966eb
Sourav <souravbumbadas25@gmail.com>
2025-03-11 23:28:34 +0000

    [FEAT] Handler Chain for Speedy

--- speedy-commons/src/main/java/com/github/silent/samurai/speedy/interfaces/Handler.java
+++
@@ -1,9 +0,0 @@
-package com.github.silent.samurai.speedy.interfaces;
-
-import com.github.silent.samurai.speedy.exceptions.SpeedyHttpException;
-
-import java.io.UnsupportedEncodingException;
-
-public interface Handler {
-    void process(RequestContext context) throws SpeedyHttpException;
-}
